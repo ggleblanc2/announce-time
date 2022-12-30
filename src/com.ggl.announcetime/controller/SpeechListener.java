5fff64a8e52ec1de76c3fcd18871f919523097ba
Gilbert G. Le Blanc <ggleblanc2@gmail.com>
2022-12-30 13:06:34 -0600

    Add files via upload
    Correct `playWavFile` method abends

--- src/com.ggl.announcetime/controller/SpeechListener.java
+++ src/com.ggl.announcetime/controller/SpeechListener.java
@@ -2,6 +2,7 @@ package com.ggl.announcetime.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalTime;
@@ -14,53 +15,65 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import com.ggl.announcetime.view.AnnounceTimeFrame;
+import com.ggl.error.display.view.ErrorDisplayDialog;
+
 public class SpeechListener implements ActionListener {
-	
+
 	private int previousHour;
-	
-	public SpeechListener() {
+
+	private final AnnounceTimeFrame view;
+
+	public SpeechListener(AnnounceTimeFrame view) {
+		this.view = view;
 		this.previousHour = -1;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		LocalTime localTime = LocalTime.now();
+//		System.out.println(localTime);
 		int minute = localTime.getMinute();
-		int hour = localTime.getHour();		// 0 to 23	
+		int hour = localTime.getHour(); // 0 to 23
 		if ((minute == 0) && (previousHour != hour)) {
 			playWavFile(hour);
 			previousHour = hour;
 		}
 	}
-	
+
 	private void playWavFile(int hour) {
 		String fileName = "/announce" + hour + ".wav";
 		InputStream inputStream = getClass().getResourceAsStream(fileName);
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(
+				inputStream);
 		AudioInputStream audioStream = null;
 		AudioFormat audioFormat = null;
-		
+
 		try {
-			audioStream = AudioSystem.getAudioInputStream(inputStream);
+			audioStream = AudioSystem.getAudioInputStream(bufferedInputStream);
 			audioFormat = audioStream.getFormat();
 		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
+			new ErrorDisplayDialog(view.getFrame(),
+					"UnsupportedAudioFileException", e);
 			return;
 		} catch (IOException e) {
-			e.printStackTrace();
+			new ErrorDisplayDialog(view.getFrame(), "IOException", e);
 			return;
 		}
-		
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class,
+				audioFormat);
 		SourceDataLine sourceLine;
 		try {
 			sourceLine = (SourceDataLine) AudioSystem.getLine(info);
 			sourceLine.open(audioFormat);
 		} catch (LineUnavailableException e) {
-			e.printStackTrace();
+			new ErrorDisplayDialog(view.getFrame(), "LineUnavailableException",
+					e);
 			return;
 		}
-        
-        sourceLine.start();
+
+		sourceLine.start();
 
 		int nBytesRead = 0;
 		byte[] abData = new byte[128000];
@@ -68,10 +81,10 @@ public class SpeechListener implements ActionListener {
 			try {
 				nBytesRead = audioStream.read(abData, 0, abData.length);
 			} catch (IOException e) {
-				e.printStackTrace();
+				new ErrorDisplayDialog(view.getFrame(), "IOException", e);
 				return;
 			}
-			
+
 			if (nBytesRead >= 0) {
 				sourceLine.write(abData, 0, nBytesRead);
 			}
@@ -79,11 +92,11 @@ public class SpeechListener implements ActionListener {
 
 		sourceLine.drain();
 		sourceLine.close();
-		
+
 		try {
 			audioStream.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			new ErrorDisplayDialog(view.getFrame(), "IOException", e);
 		}
 	}
 
